e408840f652e6b4d363d84bcf5ffcaac0b522eca
Andromeda0356 <passiton9991@gmail.com>
2025-01-08 17:59:52 +0800

    update

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -15,7 +15,7 @@ const person = {
 const newsletter = {
     display: true,
     title: <>Subscribe to {person.firstName}'s Newsletter</>,
-    description: <>I occasionally write about design, technology, and share thoughts on the intersection of creativity and engineering.</>
+    description: <>I occasionally write about design, technology, and share thoughts on the intersection of creativity.</>
 }
 
 const social = [
